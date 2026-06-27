7186ee86f327b3d95a6d773f7ba1d102eb76f955
PrincySingh04 <princys549@gmail.com>
2026-06-27 12:34:46 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 const allowedOrigins = [
-    "http://localhost:5173",
+    "https://real-estate-platform-frontend-mu.vercel.app/",
 ].filter(Boolean);
 
 app.use(cors({
