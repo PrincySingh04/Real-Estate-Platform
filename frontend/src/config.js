5368d6769d88e476e1e449fa14a1c83909427b8b
PrincySingh04 <princys549@gmail.com>
2026-06-27 15:05:15 +0530

    remove trailing slash from API_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://real-estate-platform-backend-gules.vercel.app/';
+const API_URL = 'https://real-estate-platform-backend-gules.vercel.app';
 // if you deplay here inly you have to paste the 
 // live r hosted url for backend
 export default API_URL;
